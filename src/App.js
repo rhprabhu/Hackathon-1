be47a9e4789f2f17725fd993a5277cfc0abc93af
rhprabhu <harshithaprabhu.19.1999@gmail.com>
2022-02-17 12:06:23 +0530

    Added all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Input from './components/Input';
+import {BrowserRouter as Route, Routes} from 'react-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Home />
+        <Routes>
+          <Route path="/" component={<NewGame />} />
+          <Route path="/details" component={<PlayerDetails />} />
+          <Route path="/game" component={<Game /> }/>
+          <Route path="/result" component={ <Result /> }/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
