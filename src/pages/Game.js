02965d75968232fb56094712449b43fb9f040d06
niraj <niraj.shetty@codecraft.co.in>
2022-02-17 16:06:01 +0530

    rough commit

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -6,10 +6,111 @@ const Game = () => {
   const [cells, setCells] = useState(Array(9).fill(""));
   const [winner, setWinner] = useState();
 
+  const checkForWinner = (squares) => {
+    let combos = {
+      across: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      ],
+      down: [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+      ],
+      diagnol: [
+        [0, 4, 8],
+        [2, 4, 6],
+      ],
+    };
+
+    for (let combo in combos) {
+      combos[combo].forEach((pattern) => {
+        if (
+          squares[pattern[0]] === "" ||
+          squares[pattern[1]] === "" ||
+          squares[pattern[2]] === ""
+        ) {
+          // do nothing
+        } else if (
+          squares[pattern[0]] === squares[pattern[1]] &&
+          squares[pattern[1]] === squares[pattern[2]]
+        ) {
+          setWinner(squares[pattern[0]]);
+        }
+      });
+    }
+  };
+
+  const handleClick = (num) => {
+    if (cells[num] !== "") {
+      alert("already clicked");
+      return;
+    }
+
+    let squares = [...cells];
+
+    if (turn === "x") {
+      squares[num] = "x";
+      setTurn("o");
+    } else {
+      squares[num] = "o";
+      setTurn("x");
+    }
+
+    checkForWinner(squares);
+    setCells(squares);
+  };
+
+  const handleNext = () => {
+    setWinner(null);
+    setCells(Array(9).fill(""));
+  };
+
+  const Cell = ({ num }) => {
+    return (
+      <td className={style.td} onClick={() => handleClick(num)}>
+        {cells[num]}
+      </td>
+    );
+  };
   return (
-    <div class={style.content}>
-      <div className={style.container}></div>
-      <table className={style.table}></table>
+    <div class={style.main}>
+      <div className={style.gameHeading}>
+        <span>Level Info</span>
+        <button onClick={handleNext}>NEXT</button>
+        <button>END</button>
+      </div>
+      <div className={style.playersInfo}>
+        <button>PLAYER_1</button>
+        <button>PLAYER_2</button>
+      </div>
+      <div className={style.container}>
+        <table className={style.table}>
+          <tbody>
+            <tr>
+              <Cell num={0} />
+              <Cell num={1} />
+              <Cell num={2} />
+            </tr>
+            <tr>
+              <Cell num={3} />
+              <Cell num={4} />
+              <Cell num={5} />
+            </tr>
+            <tr>
+              <Cell num={6} />
+              <Cell num={7} />
+              <Cell num={8} />
+            </tr>
+          </tbody>
+        </table>
+        {winner && (
+          <div className={style.winnerContent}>
+            <p>{winner} is the winner!</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
