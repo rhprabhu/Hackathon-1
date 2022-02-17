1b56d68c653d5d45d93a460c280a01fa8d85fe41
Priyanka <priyanka@gmail.com>
2022-02-17 17:37:57 +0530

    Updated Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,11 +22,25 @@ export default class Home extends Component {
 
         <div className="section">
           <img src="images/Group 8.png" className="bg_color" />
-         
+
           <img
             className="galaxy2_img"
             src={process.env.PUBLIC_URL + "/images/galaxy 2.svg"}
           />
+
+          <div className="form">
+            <img
+              className="speaker"
+              src={process.env.PUBLIC_URL + "/images/advertising 1.svg"}
+            />
+            <img
+              className="crush_img"
+              src={process.env.PUBLIC_URL + "/images/crush 1.svg"}
+            />
+            <button className="game-btn">New Game</button>
+            <button className="multi-btn">Multi Payer</button>
+            <button className="set-btn">Settings</button>
+          </div>
           <img
             className="galaxy1_img"
             src={process.env.PUBLIC_URL + "/images/galaxy 1.svg"}
