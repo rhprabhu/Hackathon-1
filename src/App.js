4daa2c8f8f1ca03a056d4286cbb8070a9e526aae
rhprabhu <harshithaprabhu.19.1999@gmail.com>
2022-02-17 15:19:47 +0530

    Created new game

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Route, Routes } from "react-dom";
 import NewGame from "./pages/NewGame";
 import Game from "./pages/Game";
 import Result from "./pages/Result";
-//import PlayerDetails from "./pages/PlayerDetails";
+import PlayerDetails from "./pages/PlayerDetails";
 import Home from "./components/Home";
 
 // import logo from "./images/logo.svg";
@@ -12,7 +12,9 @@ function App() {
   return (
     <div>
       {/* <img src={process.env.PUBLIC_URL + "/images/logo.svg"} /> */}
-      <Home />
+      {/* <Home /> */}
+      {/* <PlayerDetails /> */}
+      <NewGame />
       {/* <BrowserRouter>
         <Routes>
           <Route path="/" component={<NewGame />} />
